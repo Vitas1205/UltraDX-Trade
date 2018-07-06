cb33adf36e1fd61c7e0b587f9de4806d3d4a2ba4
shenqiang <qiang.shen@elementchain.org>
2018-07-06 10:39:48 +0800

    add init entrust order

---
+++ src/main/java/com/fota/trade/domain/enums/ContractOrderStatusEnum.java
@@ -0,0 +1,27 @@
+package com.fota.trade.domain.enums;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @Author: JianLi.Gao
+ * @Descripyion:
+ * @Date: Create in 下午11:29 2018/7/5
+ * @Modified:
+ */
+public enum ContractOrderStatusEnum {
+
+    PLACE_ORDER(8, "PLACE_ORDER"),
+    PARTIAL_SUCCESS(9, "PARTIAL_SUCCESS"),
+    ;
+
+    @Getter@Setter
+    private Integer code;
+    private String desc;
+
+    ContractOrderStatusEnum(Integer code, String desc) {
+        this.code = code;
+        this.desc = desc;
+    }
+}
