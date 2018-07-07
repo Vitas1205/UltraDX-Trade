6309c9d570d73d92ae0335548e6e840026c68962
shenglong.wang <shenglong.wang@emc.top>
2018-07-07 18:45:23 +0800

    update

--- src/main/java/com/fota/trade/manager/UsdkOrderManager.java
+++ src/main/java/com/fota/trade/manager/UsdkOrderManager.java
@@ -1,18 +1,26 @@
 package com.fota.trade.manager;
 
-import com.fota.client.common.BeanUtil;
 import com.fota.client.common.ResultCode;
 import com.fota.client.domain.UsdkOrderDTO;
 import com.fota.trade.domain.UsdkOrderDO;
+import com.fota.trade.domain.enums.OrderDirectionEnum;
 import com.fota.trade.domain.enums.OrderStatusEnum;
 import com.fota.trade.mapper.UsdkOrderMapper;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
+
+
+/**
+ * @Author: Harry Wang
+ * @Descripyion:
+ * @Date: Create in 2018/7/7
+ * @Modified:
+ */
 
 @Component
 @Slf4j
@@ -37,13 +45,37 @@ public class UsdkOrderManager {
         return notMatchOrderList;
     }
 
-    public ResultCode orderImp(UsdkOrderDTO usdkOrderDTO){
+    public ResultCode order(UsdkOrderDTO usdkOrderDTO){
         ResultCode resultCode = null;
-        UsdkOrderDO usdkOrderDO = BeanUtil.copy(usdkOrderDTO);
-        BigDecimal unfilledAmount = usdkOrderDTO.getTotalAmount();
-        Integer status = OrderStatusEnum.COMMIT.getCode();
-        BigDecimal fee = usdkFee;
+        UsdkOrderDO usdkOrderDO = null;
+        BeanUtils.copyProperties(usdkOrderDTO,usdkOrderDO);
+        Integer assetId = usdkOrderDO.getAssetId();
+        Long userId = usdkOrderDO.getUserId();
+        Integer orderDirection = usdkOrderDO.getOrderDirection();
+        BigDecimal price = usdkOrderDO.getPrice();
+        BigDecimal totalAmount = usdkOrderDO.getTotalAmount();
+        BigDecimal orderValue = totalAmount.multiply(price);
+        BigDecimal feeValue = orderValue.multiply(usdkFee);
+        BigDecimal totalValue = orderValue.add(feeValue);
+        if (orderDirection == OrderDirectionEnum.BID.getCode()){
+            //todo 查询usdk账户可用余额
+            //todo 判断账户可用余额是否大于tatalValue
+        }else if (orderDirection == OrderDirectionEnum.ASK.getCode()){
+            //todo 查询对应资产账户可用余额
+            //todo 判断账户可用余额是否大于tatalValue
+        }
+        usdkOrderDO.setFee(usdkFee);
+        usdkOrderDO.setStatus(OrderStatusEnum.COMMIT.getCode());
+        usdkOrderDO.setUnfilledAmount(usdkOrderDTO.getTotalAmount());
         int Ret = usdkOrderMapper.insert(usdkOrderDO);
+        if (Ret > 0){
+            resultCode = ResultCode.success();
+            //todo 放入缓存
+
+            //todo 发送RocketMQ
+        }else {
+            resultCode = ResultCode.error(1,"usdkOrder insert failed");
+        }
         return resultCode;
     }
 
