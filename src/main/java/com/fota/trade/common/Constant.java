28b9f9c2270bb363794b762a915ea9ab752f9be0
shenglong.wang <shenglong.wang@emc.top>
2018-07-17 17:25:25 +0800

    fix redis bug

--- src/main/java/com/fota/trade/common/Constant.java
+++ src/main/java/com/fota/trade/common/Constant.java
@@ -12,7 +12,7 @@ public class Constant {
     public static final int DEFAULT_PAGE_SIZE = 20;
     public static final int DEFAULT_MAX_PAGE_SIZE = 50;
     public static final String USDK_ORDER_HEAD = "fota_usdk_entrust_";
-    public static final String REDIS_KEY = "mykey"+1;
+    public static final String REDIS_KEY = "mykeys"+1;
 
     public static final BigDecimal FEE_RATE = new BigDecimal("0.01");
 
