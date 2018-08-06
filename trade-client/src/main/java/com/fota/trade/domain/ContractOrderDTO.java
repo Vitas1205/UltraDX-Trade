da02c6d10c2f679706923f9aa3e22ebc9254eaf8
shenglong.wang <shenglong.wang@emc.top>
2018-08-06 16:25:22 +0800

    add client

--- trade-client/src/main/java/com/fota/trade/domain/ContractOrderDTO.java
+++ trade-client/src/main/java/com/fota/trade/domain/ContractOrderDTO.java
@@ -4,28 +4,30 @@ import lombok.Data;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
+import java.util.Date;
 
 @Data
 public class ContractOrderDTO implements Serializable {
 
     private static final Long serialVersionUID = 2982208351452049900L;
     public Long id;
-    public Long gmtCreate;
-    public Long gmtModified;
+    public Date gmtCreate;
+    public Date gmtModified;
     public Long userId;
-    public Integer contractId;
+    public Long contractId;
     public String contractName;
     public Integer orderDirection;
     public Integer orderType;
+    public Integer operateType;
+    public Integer operateDirection;
+    public Integer lever;
     public Long totalAmount;
     public Long unfilledAmount;
-    public String price;
-    public String fee;
+    public BigDecimal price;
+    public BigDecimal fee;
     public Integer status;
     public Long matchAmount;
     public Long completeAmount;
     public Integer closeType;
-    public Integer operateType;
-    public Integer operateDirection;
     public BigDecimal averagePrice;
 }
--- trade-client/src/main/java/com/fota/trade/domain/OrderMessage.java
+++ trade-client/src/main/java/com/fota/trade/domain/OrderMessage.java
@@ -3,13 +3,29 @@ package com.fota.trade.domain;
 import lombok.Data;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
+
 @Data
 public class OrderMessage implements Serializable {
-
+  /**
+   * event
+   * 下单-1
+   * 撤单-2
+   * 成交-3
+   */
   private static final long serialVersionUID = -4615544828197773722L;
-  public int subjectId;
+  public Long subjectId;
   public String subjectName;
-  public long userId;
-  public int event;
-  public long orderId;
+  public Long userId;
+  public Integer event;
+  public Long orderId;
+  public Long askUserId;
+  public Long bidUserId;
+  public Long askOrderId;
+  public Long bidOrderId;
+  public BigDecimal amount;
+  public BigDecimal price;
+  public Long transferTime;
+  public String ip;
+  public String username;
 }
