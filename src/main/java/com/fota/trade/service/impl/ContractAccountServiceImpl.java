2791edc9b7c045169dd0cdaa1a66798360db78f3
lds <dingsun.liu@emc.top>
2018-08-25 15:36:39 +0800

    增加接口定义

---
+++ src/main/java/com/fota/trade/service/impl/ContractAccountServiceImpl.java
@@ -0,0 +1,18 @@
+package com.fota.trade.service.impl;
+
+import com.fota.common.Result;
+import com.fota.trade.domain.ContractAccount;
+import com.fota.trade.service.ContractAccountService;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by Swifree on 2018/8/25.
+ * Code is the law
+ */
+@Service
+public class ContractAccountServiceImpl implements ContractAccountService {
+    @Override
+    public Result<ContractAccount> getContractAccount(long userId) {
+        return null;
+    }
+}
