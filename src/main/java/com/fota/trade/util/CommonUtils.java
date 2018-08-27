e4be58ab0014501f9f393c8bc0c219d664a18003
lds <dingsun.liu@emc.top>
2018-08-27 17:11:29 +0800

    撮合消息控制并发

--- src/main/java/com/fota/trade/util/CommonUtils.java
+++ src/main/java/com/fota/trade/util/CommonUtils.java
@@ -1,12 +1,16 @@
 package com.fota.trade.util;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.math.BigDecimal;
 import java.util.UUID;
+import java.util.function.Supplier;
 
 /**
  * Created by Swifree on 2018/8/17.
  * Code is the law
  */
+@Slf4j
 public class CommonUtils {
     public static final BigDecimal wucha = new BigDecimal(1e-6);
 
@@ -22,6 +26,14 @@ public class CommonUtils {
         m= Math.abs(m);
         m = m % 999999999999999L;
         return m;
+    }
+    public static <R>  R exeWhitoutError(Supplier<R> supplier) {
+        try {
+            return supplier.get();
+        } catch (Throwable t) {
+            log.error("exe function error");
+        }
+        return null;
     }
 
 }
