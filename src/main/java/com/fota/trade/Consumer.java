9291af5ff2d1d419ef5bd47796eee038d088d375
lds <dingsun.liu@emc.top>
2018-08-30 11:00:41 +0800

    Revert "并发优化 step1"
    This reverts commit c245366c05084065e242c41b3e1805f81958d3af.

--- src/main/java/com/fota/trade/Consumer.java
+++ src/main/java/com/fota/trade/Consumer.java
@@ -56,7 +56,7 @@ public class Consumer {
         DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("fota-trade-testenv");
         //同样也要设置NameServer地址
         consumer.setNamesrvAddr(namesrvAddr);
-        consumer.setMaxReconsumeTimes(5);
+        consumer.setMaxReconsumeTimes(16);
         //这里设置的是一个consumer的消费策略
         //CONSUME_FROM_LAST_OFFSET 默认策略，从该队列最尾开始消费，即跳过历史消息
         //CONSUME_FROM_FIRST_OFFSET 从队列最开始开始消费，即历史消息（还储存在broker的）全部消费一遍
@@ -65,66 +65,62 @@ public class Consumer {
         consumer.setVipChannelEnabled(false);
         //设置consumer所订阅的Topic和Tag，*代表全部的Tag
         consumer.subscribe("match_order", "usdk || contract");
-        consumer.setConsumeMessageBatchMaxSize(1);
         //设置一个Listener，主要进行消息的逻辑处理
         consumer.registerMessageListener(new MessageListenerConcurrently() {
             @Override
             public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgs, ConsumeConcurrentlyContext context) {
-
                 if (CollectionUtils.isEmpty(msgs)) {
                     log.error("message error!");
                     return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
                 }
-                MessageExt messageExt = msgs.get(0);
-
-                String mqKey = messageExt.getKeys();
-                String lockKey = "LOCK_MESSAGE_KEY_"+ mqKey;
-                boolean locked = redisManager.tryLock(lockKey, Duration.ofMinutes(1));
-                if (!locked) {
-                    logFailMsg("get lock failed!", messageExt);
-                    return ConsumeConcurrentlyStatus.RECONSUME_LATER;
-                }
-                try {
-
-                    boolean isExist = redisManager.sHasKey(Constant.MQ_REPET_JUDGE_KEY_TRADE, mqKey);
-                    if (isExist) {
-                        return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+                for (MessageExt messageExt:msgs){
+                    String mqKey = messageExt.getKeys();
+                    String lockKey = "LOCK_MESSAGE_KEY_"+ mqKey;
+                    boolean locked = redisManager.tryLock(lockKey, Duration.ofMinutes(1));
+                    if (!locked) {
+                        logFailMsg("get lock failed!", messageExt);
+                        return ConsumeConcurrentlyStatus.RECONSUME_LATER;
                     }
-                    String tag = messageExt.getTags();
-                    byte[] bodyByte = messageExt.getBody();
-                    String bodyStr = null;
                     try {
-                        bodyStr = new String(bodyByte, "UTF-8");
-                    } catch (UnsupportedEncodingException e) {
-                        log.error("get mq message failed", e);
-                        return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
-                    }
-                    log.info("bodyStr()------------" + bodyStr);
-                    if (TagsTypeEnum.USDK.getDesc().equals(tag)) {
-                        UsdkMatchedOrderDTO usdkMatchedOrderDTO = JSON.parseObject(bodyStr, UsdkMatchedOrderDTO.class);
-                        ResultCode resultCode = usdkOrderService.updateOrderByMatch(usdkMatchedOrderDTO);
-                        log.info("resultCode u---------------" + resultCode);
-                        if (resultCode != null && resultCode.getCode() != null && !resultCode.isSuccess() && !(resultCode.getCode() == ILLEGAL_PARAM.getCode())) {
-                            logFailMsg(null, messageExt);
-                            return ConsumeConcurrentlyStatus.RECONSUME_LATER;
-                        }
-                    } else if (TagsTypeEnum.CONTRACT.getDesc().equals(tag)) {
-                        ContractMatchedOrderDTO contractMatchedOrderDTO = JSON.parseObject(bodyStr, ContractMatchedOrderDTO.class);
-                        ResultCode resultCode = contractOrderService.updateOrderByMatch(contractMatchedOrderDTO);
-                        log.info("resultCode c---------------" + resultCode);
-                        if (resultCode != null && resultCode.getCode() != null && !resultCode.isSuccess() && !(resultCode.getCode() == ILLEGAL_PARAM.getCode())) {
-                            logFailMsg(null, messageExt);
-                            return ConsumeConcurrentlyStatus.RECONSUME_LATER;
-                        }
-                    }
-                    redisManager.sSet(Constant.MQ_REPET_JUDGE_KEY_TRADE, mqKey);
-                } catch (Exception e) {
-                    logFailMsg(messageExt, e);
-                }finally {
-                    redisManager.releaseLock(lockKey);
-                }
 
-                logSuccessMsg(messageExt);
+                        boolean isExist = redisManager.sHasKey(Constant.MQ_REPET_JUDGE_KEY_TRADE, mqKey);
+                        if (isExist) {
+                            return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+                        }
+                        String tag = messageExt.getTags();
+                        byte[] bodyByte = messageExt.getBody();
+                        String bodyStr = null;
+                        try {
+                            bodyStr = new String(bodyByte, "UTF-8");
+                        } catch (UnsupportedEncodingException e) {
+                            log.error("get mq message failed", e);
+                            return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+                        }
+                        log.info("bodyStr()------------" + bodyStr);
+                        if (TagsTypeEnum.USDK.getDesc().equals(tag)) {
+                            UsdkMatchedOrderDTO usdkMatchedOrderDTO = JSON.parseObject(bodyStr, UsdkMatchedOrderDTO.class);
+                            ResultCode resultCode = usdkOrderService.updateOrderByMatch(usdkMatchedOrderDTO);
+                            log.info("resultCode u---------------" + resultCode);
+                            if (resultCode != null && resultCode.getCode() != null && !resultCode.isSuccess() && !(resultCode.getCode() == ILLEGAL_PARAM.getCode())) {
+                                logFailMsg(null, messageExt);
+                                return ConsumeConcurrentlyStatus.RECONSUME_LATER;
+                            }
+                        } else if (TagsTypeEnum.CONTRACT.getDesc().equals(tag)) {
+                            ContractMatchedOrderDTO contractMatchedOrderDTO = JSON.parseObject(bodyStr, ContractMatchedOrderDTO.class);
+                            ResultCode resultCode = contractOrderService.updateOrderByMatch(contractMatchedOrderDTO);
+                            log.info("resultCode c---------------" + resultCode);
+                            if (resultCode != null && resultCode.getCode() != null && !resultCode.isSuccess() && !(resultCode.getCode() == ILLEGAL_PARAM.getCode())) {
+                                logFailMsg(null, messageExt);
+                                return ConsumeConcurrentlyStatus.RECONSUME_LATER;
+                            }
+                        }
+                        redisManager.sSet(Constant.MQ_REPET_JUDGE_KEY_TRADE, mqKey);
+                    } catch (Exception e) {
+                        logFailMsg(messageExt, e);
+                    }finally {
+                        redisManager.releaseLock(lockKey);
+                    }
+                }
                 return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
             }
         });
@@ -133,16 +129,6 @@ public class Consumer {
         System.out.println("Consumer Started.");
     }
 
-    private void logSuccessMsg(MessageExt messageExt) {
-        String body = null;
-        try {
-            body = new String(messageExt.getBody(), "UTF-8");
-        } catch (UnsupportedEncodingException e) {
-            log.error("get mq message failed", e);
-        }
-        log.error("consume message success, msgId={}, msgKey={}, tag={},  body={}, reconsumeTimes={}", messageExt.getMsgId(), messageExt.getKeys(), messageExt.getTags(),
-                body, messageExt.getReconsumeTimes());
-    }
     private void logFailMsg(MessageExt messageExt, Throwable t) {
         String body = null;
         try {
