b2408c8f44ec7ce74675de1e2a7d3dffb014f55a
shenglong.wang <shenglong.wang@emc.top>
2018-08-31 15:48:56 +0800

    fix bugs

---
+++ src/test/java/com/fota/trade/manager/ContractMatchOrderMapperTest.java
@@ -0,0 +1,49 @@
+package com.fota.trade.manager;
+
+import com.fota.trade.domain.ContractMatchedOrderDO;
+import com.fota.trade.mapper.ContractMatchedOrderMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author: Harry Wang
+ * @Descripyion:
+ * @Date: Create in 2018/8/31 14:56
+ * @Modified:
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Slf4j
+@Transactional
+public class ContractMatchOrderMapperTest {
+
+    @Autowired
+    private ContractMatchedOrderMapper contractMatchedOrderMapper;
+
+    @Test
+    public void listByUserIdTest(){
+        Long userId = 285L;
+        List<Long> contractIds = new ArrayList<>();
+        contractIds.add(1000L);
+        List<ContractMatchedOrderDO> list = contractMatchedOrderMapper.listByUserId
+                (userId, contractIds, 0,20,null,null);
+        log.info("_____"+list);
+    }
+
+    @Test
+    public void countByUserIdTest(){
+        Long userId = 285L;
+        List<Long> contractIds = new ArrayList<>();
+        contractIds.add(1000L);
+        int ret = contractMatchedOrderMapper.countByUserId(userId, contractIds, null, null);
+        log.info("----"+ret);
+    }
+}
