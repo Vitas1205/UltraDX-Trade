d425802b8940828ce020939fca1520a6c898faf7
shenglong.wang <shenglong.wang@emc.top>
2018-08-31 20:11:01 +0800

    fix position select bugs

--- src/test/java/com/fota/trade/service/UserPositionServiceTest.java
+++ src/test/java/com/fota/trade/service/UserPositionServiceTest.java
@@ -1,6 +1,8 @@
 package com.fota.trade.service;
 
+import com.fota.trade.domain.UserPositionDTO;
 import com.fota.trade.domain.query.UserPositionQuery;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,6 +11,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * @author Gavin Shen
@@ -17,6 +20,7 @@ import javax.annotation.Resource;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @Transactional
+@Slf4j
 public class UserPositionServiceTest {
 
     @Resource
@@ -29,6 +33,12 @@ public class UserPositionServiceTest {
         userPositionQuery.setContractId(1001L);
         com.fota.common.Page<com.fota.trade.domain.UserPositionDTO> page = userPositionService.listPositionByQuery(482,100, 1, 10 );
 //        Assert.assertTrue(null != page && null != page.getData());
+    }
+
+    @Test
+    public void listPositionByUserIdTest(){
+        List<UserPositionDTO> list = userPositionService.listPositionByUserId(285L);
+        log.info("----"+list.size());
     }
 
 
