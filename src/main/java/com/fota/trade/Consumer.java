29d3207a0693c3f9c5160388a4282267b56f55a5
lds <dingsun.liu@emc.top>
2018-09-23 20:49:07 +0800

    去重fix

--- src/main/java/com/fota/trade/Consumer.java
+++ src/main/java/com/fota/trade/Consumer.java
@@ -87,7 +87,7 @@ public class Consumer {
                 String existKey = MQ_REPET_JUDGE_KEY_MATCH  + mqKey;
                 //判断是否已经成交
                 boolean locked = redisManager.tryLock(existKey, Duration.ofDays(1));
-                if (locked) {
+                if (!locked) {
                     logSuccessMsg(messageExt, "already consumed, not retry");
                     return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
                 }
