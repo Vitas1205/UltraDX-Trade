b1e5935e31ec7491d46a3627093f9a3a1967e79c
lds <dingsun.liu@emc.top>
2018-11-26 20:02:36 +0800

    交易枚举类

---
+++ src/main/java/com/fota/trade/common/TradeBizTypeEnum.java
@@ -0,0 +1,14 @@
+package com.fota.trade.common;
+
+/**
+ * Created by lds on 2018/11/26.
+ * Code is the law
+ */
+public enum  TradeBizTypeEnum {
+    COIN_ORDER,
+    COIN_CANCEL_ORDER,
+    COIN_MATCH,
+    CONTRACT_ORDER,
+    CONTRACT_CANCEL_ORDER,
+    CONTRACT_MATCH
+}
