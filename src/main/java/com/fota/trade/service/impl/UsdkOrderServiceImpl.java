9cecf315269bb859d382790d7e795ab63c74e5e3
lds <dingsun.liu@emc.top>
2018-12-25 14:50:27 +0800

    撤单log优化

--- src/main/java/com/fota/trade/service/impl/UsdkOrderServiceImpl.java
+++ src/main/java/com/fota/trade/service/impl/UsdkOrderServiceImpl.java
@@ -233,11 +233,6 @@ public class UsdkOrderServiceImpl implements UsdkOrderService {
         List<Long> orderIds = cancelOrderRequest.getOrderIds();
         try {
             result = usdkOrderManager.batchCancelOrder(userId, orderIds);
-            if (result.isSuccess()) {
-                for (Long orderId : orderIds){
-                    tradeLog.info("撤销@@@" + userId+ "@@@" + orderId);
-                }
-            }
             return result;
         }catch (Exception e){
             log.error("USDK batchCancel() failed", e);
@@ -259,9 +254,6 @@ public class UsdkOrderServiceImpl implements UsdkOrderService {
         ResultCode resultCode = new ResultCode();
         try {
             resultCode = usdkOrderManager.cancelOrder(userId, orderId, userInfoMap);
-            if (resultCode.isSuccess()) {
-                tradeLog.info("撤销@@@" + userId+ "@@@" + orderId);
-            }
             return resultCode;
         }catch (Exception e){
             log.error("USDK cancelOrder() failed", e);
