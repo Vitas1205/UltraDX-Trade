b66e87b707b3401d0243f1c0310f58d14d5641bd
zx <2211088929@qq.com>
2021-05-25 15:20:24 +0800

    update

---
+++ src/main/java/com/fota/trade/common/MatchedOrderConstants.java
@@ -0,0 +1,28 @@
+package com.fota.trade.common;
+
+/**
+ * @Author: Carl Zhang
+ * @Descripyion:
+ * @Date: Create in 下午 16:23 2018/8/23 0023
+ * @Modified:
+ */
+public class MatchedOrderConstants {
+    public static final String FOTA_TRADE_MATCHEDUSDKINDEX = "fota_trade_matchedUsdkIndex_";
+
+    public static final String CACHE_KEY_MATCH_USDK = "CACHE_KEY_MATCH_USDK_";
+
+    public static final String FOTA_TRADE_MATCHEDCONTRACTINDEX = "fota_trade_matchedContractIndex_";
+
+    public static final String CACHE_KEY_MATCH_CONTRACT = "CACHE_KEY_MATCH_CONTRACT_";
+
+    public static final String FOTA_LATEST_CONTRACT_MATCHED_ORDER = "FOTA_LATEST_CONTRACT_MATCHED_ORDER_";
+
+    public static final String FOTA_LATEST_USDT_MATCHED_ORDER = "FOTA_LATEST_USDT_MATCHED_ORDER_";
+
+    public static final int CONTRACT_FEE_PRECISION = 8;
+
+    public static final String FOTA_RESTORE_USDT_MATCHED_MAP = "FOTA_RESTORE_USDT_MATCHED_MAP";
+
+    public static final String FOTA_RESTORE_CONTRACT_MATCHED_MAP = "FOTA_RESTORE_CONTRACT_MATCHED_MAP";
+
+}
--- src/main/java/com/fota/trade/task/TradeAmountStatisticTask.java
+++ src/main/java/com/fota/trade/task/TradeAmountStatisticTask.java
@@ -3,24 +3,30 @@ package com.fota.trade.task;
 import com.fota.account.domain.UserVipDTO;
 import com.fota.account.service.UserVipService;
 import com.fota.asset.domain.UserCapitalDTO;
-import com.fota.asset.domain.enums.AssetTypeEnum;
 import com.fota.asset.service.AssetService;
+import com.fota.common.domain.config.Asset;
+import com.fota.common.domain.config.BrokerTradingPairConfig;
+import com.fota.common.domain.enums.AssetTypeEnum;
+import com.fota.common.manager.BrokerTradingPairManager;
+import com.fota.common.manager.FotaAssetManager;
+import com.fota.trade.common.MatchedOrderConstants;
 import com.fota.trade.domain.UsdkMatchedOrderDO;
+import com.fota.trade.manager.RedisManager;
 import com.fota.trade.mapper.sharding.UsdkMatchedOrderMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
 
 import javax.annotation.Resource;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Component
@@ -36,10 +42,14 @@ public class TradeAmountStatisticTask {
     @Autowired
     private UserVipService userVipService;
 
-    /**
-     * todo 统计的币种
-     */
-    private final Long assetId = (long) AssetTypeEnum.TWD.getCode();
+    @Autowired
+    private FotaAssetManager fotaAssetManager;
+
+    @Resource
+    private RedisManager redisManager;
+
+    @Autowired
+    private BrokerTradingPairManager brokerTradingPairManager;
 
     /**
      * 每天0时统计30天内交易总量和平台币锁仓量
@@ -48,33 +58,95 @@ public class TradeAmountStatisticTask {
     @Scheduled(cron = "0 0/30 * * * ?")
     public void tradeAmountStatistic() {
         log.info("tradeAmountStatistic task start!");
-        List<UserCapitalDTO> userCapitalDTOList = assetService.getUserCapital(assetId);
+        List<Asset> assets = fotaAssetManager.getAllAssets();
+        List<UserCapitalDTO> userCapitalDTOList = new ArrayList<>();
+        for(Asset asset : assets){
+            List<UserCapitalDTO> subUserCapitalDTOList = assetService.getUserCapital(asset.getId());
+            if(!CollectionUtils.isEmpty(subUserCapitalDTOList)){
+                userCapitalDTOList.addAll(subUserCapitalDTOList);
+            }
+        }
         log.info("userCapitalDTOList:{}",userCapitalDTOList);
-        userCapitalDTOList.forEach(
-                e->{
-                    Long userId = e.getUserId();
+        Map<Long, List<UserCapitalDTO>> map = userCapitalDTOList.stream().collect(Collectors.groupingBy(UserCapitalDTO::getUserId));
+        for(Map.Entry<Long, List<UserCapitalDTO>> entry : map.entrySet()){
+            Long userId = entry.getKey();
+            List<UserCapitalDTO> list = entry.getValue();
 
-                    BigDecimal canUsedAmount = new BigDecimal(e.getAmount())
-                            .subtract(new BigDecimal(e.getLockedAmount()))
+            BigDecimal canUsedAmount = BigDecimal.ZERO;
+            BigDecimal tradeAmount30days = BigDecimal.ZERO;
+            for(UserCapitalDTO userCapitalDTO : list) {
+                if(userCapitalDTO.getAssetId().equals(AssetTypeEnum.TWD.getCode())) {
+                    canUsedAmount = new BigDecimal(userCapitalDTO.getAmount())
+                            .subtract(new BigDecimal(userCapitalDTO.getLockedAmount()))
                             .setScale(4, RoundingMode.HALF_UP);
-
-                    Long nowTime = LocalDateTime.now().toEpochSecond(ZoneOffset.of("+8"));
-                    Long startTime = nowTime - 30*24*60*60;
-                    List<UsdkMatchedOrderDO> usdkMatchedOrderDOList = usdkMatchedOrderMapper.listByUserId(userId, Collections.singletonList(assetId),0,Integer.MAX_VALUE,startTime,nowTime);
-
-                    BigDecimal tradeAmount30days = usdkMatchedOrderDOList.stream()
-                            .map(x->x.getFilledAmount().multiply(x.getFilledPrice()))
-                            .reduce(BigDecimal.ZERO,BigDecimal::add)
-                            .setScale(4,RoundingMode.HALF_UP);
-
-                    UserVipDTO userVipDTO = new UserVipDTO();
-                    userVipDTO.setUserId(userId);
-                    userVipDTO.setTradeAmount30days(tradeAmount30days.toPlainString());
-                    userVipDTO.setLockedAmount(canUsedAmount.toPlainString());
-                    log.info("userVipDTO:{}",userVipDTO);
-                    userVipService.updateByUserId(userVipDTO);
                 }
-        );
+                Long nowTime = LocalDateTime.now().toEpochSecond(ZoneOffset.of("+8"));
+                Long startTime = nowTime - 30 * 24 * 60 * 60;
+                List<UsdkMatchedOrderDO> usdkMatchedOrderDOList = usdkMatchedOrderMapper.listByUserId(userId, null, 0, Integer.MAX_VALUE, startTime, nowTime);
+                tradeAmount30days = usdkMatchedOrderDOList.stream()
+                        .map(x -> x.getFilledAmount().multiply(x.getFilledPrice()).multiply(getRateByAssetName(x.getAssetName())))
+                        .reduce(BigDecimal.ZERO, BigDecimal::add)
+                        .setScale(4, RoundingMode.HALF_UP);
 
+            }
+
+            UserVipDTO userVipDTO = new UserVipDTO();
+            userVipDTO.setUserId(userId);
+            userVipDTO.setTradeAmount30days(tradeAmount30days.toPlainString());
+            userVipDTO.setLockedAmount(canUsedAmount.toPlainString());
+            log.info("userVipDTO:{}",userVipDTO);
+            userVipService.updateByUserId(userVipDTO);
+        }
+    }
+
+    private BigDecimal getRateByAssetName(String assetName){
+        Long brokerId = 508090L;
+        HashMap<String, BigDecimal> rateMap = getExchangeRate(brokerId);
+        log.info("getExchangeRate:{}",rateMap);
+        return rateMap.get(assetName+"/TWD");
+    }
+
+    private HashMap<String, BigDecimal> getExchangeRate(Long brokerId){
+        HashMap<String, BigDecimal> exchangeRateMap = new HashMap<>();
+        Collection<BrokerTradingPairConfig> allTradingPairList = brokerTradingPairManager.listAllTradingPairsByBrokerId(brokerId);
+        if (brokerId > 1){
+            for (BrokerTradingPairConfig temp : allTradingPairList) {
+                try {
+                    String rate = redisManager.get(MatchedOrderConstants.FOTA_LATEST_USDT_MATCHED_ORDER +
+                            temp.getId());
+                    rate = rate != null ? rate : BigDecimal.ZERO.toPlainString();
+                    BigDecimal rateBd = new BigDecimal(rate);
+                    exchangeRateMap.put(temp.getName(), rateBd);
+                }catch (Exception e){
+                    log.error("getExchangeRate error, BrokerTradingPairConfig:{}, brokerId:{}", temp, brokerId, e);
+                }
+            }
+        } else {
+            //主站逻辑
+            Long assetToBtcId = null;
+            for (BrokerTradingPairConfig temp : allTradingPairList) {
+                try {
+                    String baseName = brokerTradingPairManager.getTradingPairById(temp.getId()).getBaseName();
+                    for (BrokerTradingPairConfig brokerTradingPairConfig : allTradingPairList){
+                        if (brokerTradingPairConfig.getBaseName().equals(baseName) && brokerTradingPairConfig.getQuoteName().equals(com.fota.asset.domain.enums.AssetTypeEnum.BTC.getDesc())){
+                            assetToBtcId = brokerTradingPairConfig.getId();
+                        }
+                    }
+                    if (assetToBtcId == null){
+                        continue;
+                    }
+                    String rate = redisManager.get(MatchedOrderConstants.FOTA_LATEST_USDT_MATCHED_ORDER +
+                            assetToBtcId);
+                    rate = rate != null ? rate : BigDecimal.ZERO.toPlainString();
+                    BigDecimal rateBd = new BigDecimal(rate);
+                    exchangeRateMap.put(temp.getBaseName(), rateBd);
+                } catch (Exception e){
+                    log.error("getExchangeRate error, BrokerTradingPairConfig:{}, brokerId:{}", temp, brokerId, e);
+                }
+            }
+            exchangeRateMap.put(com.fota.asset.domain.enums.AssetTypeEnum.BTC.getDesc(), new BigDecimal("1"));
+        }
+
+        return exchangeRateMap;
     }
 }
