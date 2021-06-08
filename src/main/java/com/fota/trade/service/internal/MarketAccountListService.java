8781858aa4f4409191a4c91517507d2b5337ad81
amumuku <1527528140@qq.com>
2021-06-08 17:11:29 +0800

    提交发现做市账号问题

--- src/main/java/com/fota/trade/service/internal/MarketAccountListService.java
+++ src/main/java/com/fota/trade/service/internal/MarketAccountListService.java
@@ -1,9 +1,12 @@
 package com.fota.trade.service.internal;
 
+import com.alibaba.fastjson.JSON;
 import com.fota.trade.config.MarketAccountListConfig;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Slf4j
 @Service
@@ -12,6 +15,8 @@ public class MarketAccountListService {
     private MarketAccountListConfig marketAccountListConfig;
 
     public boolean contains(Long userId) {
+        List<Long> marketAccountList = marketAccountListConfig.getMarketAccountList();
+        log.info("contains marketAccountList:{}", JSON.toJSONString(marketAccountList));
         return marketAccountListConfig.getMarketAccountList() != null
                 && marketAccountListConfig.getMarketAccountList().contains(userId);
     }
